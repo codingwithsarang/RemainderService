e362d71e720b4a4d6a7b275406fe72cd04640785
sarang joshi <sarangjoshi196@gmail.com>
2024-05-24 00:30:22 +0530

    added notification ticket model

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 
 const bodyParser = require('body-parser')
 const {PORT} = require('./config/serverConfig')
-const { sendBasicEmail } = require('./services/emailService')
 
 
 function setupServer(){
@@ -10,12 +9,6 @@ function setupServer(){
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended: true}))
 
-    sendBasicEmail(
-        'Support@gmail.com',
-        'sarangjoshi196@gmail.com',
-        'checking the email service working or not',
-        'hey, how are you!'
-    )
     app.listen(PORT,()=>{
         console.log('server is running on port no',PORT)
     })
