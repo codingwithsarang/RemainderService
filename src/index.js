cc2587f589cbc92c6445e094e75bb295c92ad5b0
sarang joshi <sarangjoshi196@gmail.com>
2024-05-23 19:51:20 +0530

    setup the remainder service

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const bodyParser = require('body-parser')
+const {PORT} = require('./config/serverConfig')
+
+
+function setupServer(){
+    const app = express()
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended: true}))
+
+
+    app.listen(PORT,()=>{
+        console.log('server is running on port no',PORT)
+    })
+}
+
+
+setupServer()
